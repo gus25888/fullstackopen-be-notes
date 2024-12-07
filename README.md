5896557c24c1eb0a410ef7ae48d986b30f7b3655
Gustavo Baeza Avello <gustavobaezaavello@gmail.com>
2024-12-06 22:00:31 -0300

    Implementación de concepto de pruebas unitarias con node:test

--- README.md
+++ README.md
@@ -1,14 +1,15 @@
-# fullstackopen-be-notes
+# backend-notes
 
 Notes backend app
 
 ## Iniciar
 
 ```sh
-npm clone <https://github.com/gus25888/fullstackopen-notes>
+npm clone https://github.com/gus25888/fullstackopen-notes
 npm install
 ```
 
-Luego, crear un archivo .env en la raíz, con las siguientes variables:
-    MONGODB_URI
-    PORT
+Luego, crear un archivo `.env` en la raíz, con las siguientes variables:
+
+- MONGODB_URI
+- PORT
---
+++ tests/reverse.test.js
@@ -0,0 +1,26 @@
+const { test } = require('node:test')
+const assert = require('node:assert')
+
+const reverse = require('../utils/for_testing').reverse
+
+test('reverse of a', () => {
+  const result = reverse('a')
+
+  assert.strictEqual(result, 'a')
+})
+
+test('reverse of react', () => {
+  const result = reverse('react')
+
+  //Resultado de prueba incorrecto
+  // assert.strictEqual(result, 'tkaer')
+
+  //Resultado de prueba correcto
+  assert.strictEqual(result, 'tcaer')
+})
+
+test('reverse of saippuakauppias', () => {
+  const result = reverse('saippuakauppias')
+
+  assert.strictEqual(result, 'saippuakauppias')
+})
